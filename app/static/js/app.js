f915467d0fea864406abf000913b9601303f9272
Richardo Stewart <user@Richardos-MacBook-Pro.local>
2019-03-31 22:57:20 -0500

    Lab 7

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,4 +1,45 @@
 /* Add your Application JavaScript */
+const Upload = Vue.component('upload-form', {
+    template: `
+    <div>
+        <form id = "uploadForm" method= 'POST' enctype= "multipart/form-data" @submit.prevent= "uploadPhoto" action="/api/upload">
+
+            <label>Description</label><br>
+            <textarea class="form-control"></textarea> <br>
+            <label>Photo Upload</label><br>
+            <input type= "file"><br><br>
+            <button class="btn btn-primary "type ="submit">Submit</button>
+        
+        </form>
+    </div>
+    
+    `,
+    uploadPhoto: function(){
+
+            let uploadForm = document.getElementById('uploadForm');
+            let form_data = new FormData(uploadForm);
+
+            fetch("/api/upload",{
+                method: 'POST',
+                body: form_data,
+                headers: {
+                    'X-CSRFToken': token
+                },
+                credentials: 'same-origin'
+            })
+            .then(function (response){
+                return response.json();
+            })
+            .then(function (jsonResponse){
+                console.log(jsonResponse);
+            })
+            .catch(function(error){
+                console.log(error);
+            });
+        }
+
+    });
+
 Vue.component('app-header', {
     template: `
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
@@ -11,6 +52,9 @@ Vue.component('app-header', {
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
             <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/upload">Upload Form <span class="sr-only"></span></router-link>
           </li>
         </ul>
       </div>
@@ -57,6 +101,7 @@ const router = new VueRouter({
     routes: [
         {path: "/", component: Home},
         // Put other routes here
+        {path: "/upload", component: Upload},
         // This is a catch all route in case none of the above matches
         {path: "*", component: NotFound}
     ]
